1efe1d0a3287ba89d804832bfd246f9bb22a105b
Alex <alexsandrolima991@hotmail.com>
2023-05-16 00:46:20 -0300

    implements tests in class Dish

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,29 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    instance = Dish("Alexoa", 55.98)
+
+    assert instance.name == "Alexoa"
+    assert instance.price == 55.98
+    assert instance.__repr__() == "Dish('Alexoa', R$55.98)"
+    assert instance.__hash__() == hash("Dish('Alexoa', R$55.98)")
+    assert instance == Dish("Alexoa", 55.98)
+
+    instance.add_ingredient_dependency(Ingredient('cacau'), 5)
+
+    assert instance.recipe == {Ingredient('cacau'): 5}
+    assert not instance.get_restrictions()
+
+    assert instance.get_ingredients() == {Ingredient('cacau')}
+
+    with pytest.raises(TypeError, match="Dish price must be float."):
+        Dish("Alexoa", "a")
+
+    with pytest.raises(
+        ValueError, match="Dish price must be greater then zero"
+    ):
+        Dish("Alexoa", -1)
